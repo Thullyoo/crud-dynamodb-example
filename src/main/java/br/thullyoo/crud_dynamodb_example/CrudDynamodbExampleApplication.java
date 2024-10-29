ca15b55fecca8d1caa30dfff94bc4d811fc619db
Thullyoo <thullyofilho@gmail.com>
2024-10-29 15:00:58 -0300

    Initial commit

---
+++ src/main/java/br/thullyoo/crud_dynamodb_example/CrudDynamodbExampleApplication.java
@@ -0,0 +1,13 @@
+package br.thullyoo.crud_dynamodb_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDynamodbExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDynamodbExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/thullyoo/crud_dynamodb_example/CrudDynamodbExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.thullyoo.crud_dynamodb_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDynamodbExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
